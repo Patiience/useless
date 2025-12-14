69e9551eca2e83d22d51072298f1823f17b8c102
Philip Huang <phileepa@Guipings-MacBook-Air.local>
2025-12-13 19:54:09 -0500

    main, homePage.ejs updates

--- main.js
+++ main.js
@@ -1,91 +1,35 @@
-const express = require("express");
-const app = express();
+const express = require("express"); 
 const path = require("path");
-const portNumber = 7003;
-const bodyParser = require("body-parser");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express(); 
+const portNumber = 3000;
 
-app.use(bodyParser.urlencoded({ extended: false }));
+//const browse = require("./routes/browse");
+//const fact = require("./routes/fact");
+
+/* Defining the view/templating engine to use */
 app.set("view engine", "ejs");
+
+/* Directory where templates will reside */
 app.set("views", path.resolve(__dirname, "templates"));
 
-/*
-require("dotenv").config({
-   path: path.resolve(__dirname, "credentialsDontPost/.env"),
-});
-*/
+app.use(express.static(__dirname));
 
-const databaseName = "CMSC335DB";
-const collectionName = "moviesCollection";
-const uri = process.env.MONGO_CONNECTION_STRING;
-const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
+/* Treating building.js like middleware. Use buildings.js 
+   file to handle endpoints that start with /buildings */
+//app.use("/browse", browse);
 
-app.get("/", async (req, res) => {
-   res.send(`My Deployment`);
-});
+/* Treating schoolDorms.js like middleware. Use schoolDorms.js 
+   file to handle endpoints that start with /dorms.  Examples shows you
+   don't have to name file after part of the endpoint */
+//app.use("/fact", fact);
 
-app.get("/insertMovies", async (req, res) => {
-    try {
-      await client.connect();
-      const collection = client.db(databaseName).collection(collectionName);
-      
-      /* Inserting several movies */
-      const moviesArray = [
-         { name: "Batman", year: 2021, stars: 1.5 },
-         { name: "Wonder Women", year: 2005, stars: 2.0 },
-         { name: "When Harry Met Sally", year: 1985, stars: 5 },
-         { name: "Hulk", year: 1985, stars: 5 },
-      ];
-      result = await collection.insertMany(moviesArray);
-      res.send(`<h2>Inserted ${result.insertedCount} movies</h2>`);
-      /* Comment Here */
-   } catch (e) {
-      console.error(e);
-   } finally {
-      await client.close();
-   }
-});
+/* This one cannot be first */
+app.use("/", (request, response) => {
+   response.render("homePage")
+ });
 
-app.get("/listMovies", async (req, res) => {
-   
-    try {
-      await client.connect();
-      const collection = client.db(databaseName).collection(collectionName);
-      
-      /* Listing all movies */
-      const filter = {};
-      cursor = collection.find(filter);
-      result = await cursor.toArray();
-      let answer = "";
-      result.forEach(elem => answer += `${elem.name} (${elem.year})<br>`);
-      answer += `Found: ${result.length} movies`; 
-      res.send(answer);
-   } catch (e) {
-      console.error(e);
-   } finally {
-      await client.close();
-   }
-});
-
-app.get("/clearCollection", async (req, res) => {
-    try {
-      await client.connect();
-      
-      const collection = client.db(databaseName).collection(collectionName);
-      const result = await collection.drop();
-      res.send("<h2>Collection Cleared</h2>");
-   } catch (e) {
-      console.error(e);
-   } finally {
-      await client.close();
-   }
-});
-
-app.get("/getSummary", (req, res) => {
-   const variables = { year: 2025 };
-   res.render("summary", variables);
-});
-
+console.log(`Server listening on port ${portNumber}`)
+const homeURL = `http://localhost:${portNumber}`;
+console.log(homeURL);
 
 app.listen(portNumber);
-console.log(`main URL http://localhost:${portNumber}/`);
